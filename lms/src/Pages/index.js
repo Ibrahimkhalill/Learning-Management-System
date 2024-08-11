e0a6aa05cc734697b77aae678b3f3110b96e6b1f
Ibrahim Khalil <83811210+Ibrahimkhalill@users.noreply.github.com>
2024-08-11 17:58:33 +0600

    Delete lms directory

--- lms/src/Pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as HomePage } from "./HomePage";
-export { default as Search } from "./Search";
-export {default as MyCourses} from "./MyCourses";
-export {default as Profile} from "./Profile";
